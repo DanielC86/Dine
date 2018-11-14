56e56dc7bcd5a8210ad66984c5e8053312ed9648
Daniel <daniel.czajka@gmail.com>
2018-11-14 23:14:30 +0000

    adding more UI and adding functionality to the buttons (for example going from main activity to the second, from second to Main, from second to the third)

--- app/src/main/java/com/example/danie/dine/MainActivity.java
+++ app/src/main/java/com/example/danie/dine/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.danie.dine;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //setting exit button with function
+        Button btnExit = (Button) findViewById(R.id.btnExit);
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        Button btnOneTwo = (Button) findViewById(R.id.btnOneTwo);
+        btnOneTwo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent OneTwoIntent = new Intent(getApplicationContext(),StepTwo.class);
+                //passing info to the second activity
+                startActivity(OneTwoIntent);
+                finish();
+
+            }
+        });
 
         //test
 
--- app/src/main/java/com/example/danie/dine/StepTwo.java
+++ app/src/main/java/com/example/danie/dine/StepTwo.java
@@ -1,7 +1,10 @@
 package com.example.danie.dine;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class StepTwo extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class StepTwo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_two);
+
+        Button btnTwoOne = (Button)findViewById(R.id.btnTwoOne);
+        btnTwoOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent TwoOneIntent = new Intent(getApplicationContext(),MainActivity.class);
+                //passing info to the second activity
+                startActivity(TwoOneIntent);
+                finish();
+            }
+        });
+
+        Button btnTwoThree = (Button)findViewById(R.id.btnTwoThree);
+        btnTwoThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent TwoThreeIntent = new Intent(getApplicationContext(),StepThreeSuccess.class);
+                //passing info to the third activity
+                startActivity(TwoThreeIntent);
+                finish();
+            }
+        });
     }
 }
